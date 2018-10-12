12733a03090f360e4538047d2f2122f769854643
Alex <j.alexander.lehman@gmail.com>
2018-10-12 16:50:03 -0400

    changes

--- P1-2 Redux.asm
+++ P1-2 Redux.asm
@@ -17,7 +17,7 @@
 # $13 = Number of values in mainArr
 # $14 = For evaluate function
 # $15 = Value on nonsense square
-# $17 = Memory Location of exit 
+# $17 = Value above a nonsense square
 # $18 = Total size of neighbor values and for other stuff
 # #19 = Value of nearArr(i)
 # $20 = Array index-er
@@ -27,9 +27,12 @@
 # $27 = 238 (Number of closed square)
 # $28 = value of neighbor square
 .data
-			mainArr: .word 0x0A0A0A0A, 0x0A0A0A0A, 0xEE0A0A0A, 0xEEEEEEEE, 0x0AEEEEEE, 0xEEEEEE0A, 0xEEEEEEEE, 0xEE0A0AEE, 0xEEEEEEEE, 0x0AEEEEEE, 0xEEEEEE0A, 0xEEEEEEEE, 0xEE0A0AEE, 0xEEEEEEEE, 0x0AEEEEEE, 0xEEEEEE0A, 0xEEEEEEEE, 0xEE0A0AEE, 0xEEEEEEEE, 0x0AEEEEEE, 0xEEEEEE0A, 0xEEEEEEEE, 0x0A0A0AEE, 0x0A0A0A0A, 0x0A0A0A0A
-			nearArr: .word 0xEEEEEEEE, 0xEEEEEEEE    # Array of neighbor squares' values
-			indexArr: .word 0xEEEEEEEE, 0xEEEEEEEE     # Array of neighbor squares' indices 
+			mainArr: .word 	0x0B0B0B0B, 0x0B0B0B0B, 0xEE0B0B0B, 0xEEEEEEEE, 0x0BEEEEEE, 0xEEEEEE0B, 0xEEEEEEEE, 0xEE0B0BEE, 0xEEEEEEEE, 0x0BEEEEEE, 0xEEEEEE0B, 0xEEEEEEEE, 0xEE0B0BEE, 0xEEEEEEEE, 0x0BEEEEEE, 0xEEEEEE0B, 0xEEEEEEEE, 0xEE0B0BEE, 0xEEEEEEEE, 0x0BEEEEEE, 0xEEEEEE0B, 0xEEEEEEEE, 0x0B0B0BEE, 0x0B0B0B0B, 0x0B0B0B0B
+			nearArr: .word 	0xEEEEEEEE, 0xEEEEEEEE   	# Array of neighbor squares' values
+			indexArr: .word 0xEEEEEEEE, 0xEEEEEEEE     	# Array of neighbor squares' indices 
+			neighArr: .word 0xFFF7F6F5, 0x0B0A0901    	# Array of neighbor squares' change values 
+			subArr: .word 	0x313B454F, 0x09131D27		# Array of values to compare for subtraction 
+			subvalsArr: .word 	0xEDEBE9E7, 0xF5F3F1EF	# Array of values to subtract 
 .text
 			# First code run
 			swi   567	   	        # Bury mines (returns # buried in $1)
@@ -38,11 +41,12 @@
 			addi $9, $0, 9			# Initialize $9 to be the value assigned to flagged squares	
 			add $13, $0, $0
 			addi $15, $0, 10		# Assign 10 to $15 to denote "Fully known" squares
+			addi $16, $0, 11
+			add $17, $31, $0	    # Save memory link
 			add $21, $0, $0
 			addi $26, $0, 11			# Initialize $26 to zero
 			addi $27, $0, 238		# Initialize $27 to 0
-			add $17, $31, $0	    # Save memory link
-
+			
 			# INITIAL GUESS
 			add  $2, $0, $0		# Mine field position 0
             addi  $3, $0, -1        # Guess
@@ -57,7 +61,7 @@ skip2:		sb $4, mainArr($26)		# store the returned value into the master array
 
 Guess:		addi $26, $26, 1
 			lbu $24, mainArr($26)
-			bne $24, $27, Guess		# If the index of mainArr at $23 isnt FF, then loop until it does
+			bne $24, $27, Guess		# If the index of mainArr at $24 isnt FF, then loop until it does
 			add $11, $26, $0
 			jal subtract
 			add  $2, $0, $11		# Mine field position 0
@@ -80,34 +84,27 @@ end:		jr  $17  	  		    # return to OS - ENDS
 	
 			# CHOOSES WHICH SQUARES TO FLAG/OPEN
 resetTen: 	add $10, $0, $0
-			addi $12, $0, 11
+			addi $12, $0, 10
+endNot:		addi $12, $12, 1
 FlagOrOpen:	lbu $11, mainArr($12)	# Load current neighbor value into $11
-			beq $11, $15, endif		# If $11 is an evaluated number, skip
+			beq $11, $16, endNot
+			beq $11, $27, endNot
 			beq $11, $9, endif		# If $11 is an evaluated number, skip
+			beq $11, $15, endif		# If $11 is an evaluated number, skip
 			bne $11, $0, next		# Check if $11 is 0
 			beq $11, $21, endif		# If $11 is 0 for Flag mode, skip
 
 next:		add $20, $0, $0
 			add $8, $0, $0
-			addi $28, $12, -11
+			add $6, $0, $0
+neighLoop:	lb $28, neighArr($20)
+			add $28, $12, $28
 			jal neighIndex
-			addi $28, $12, -10
-			jal neighIndex
-			addi $28, $12, -9
-			jal neighIndex
-			addi $28, $12, -1
-			jal neighIndex
-			addi $28, $12, 1
-			jal neighIndex
-			addi $28, $12, 9
-			jal neighIndex
-			addi $28, $12, 10
-			jal neighIndex
-			addi $28, $12, 11
-			jal neighIndex
-			add $20, $0, $0
+			slti $6, $20, 8
+			bne $6, $0, neighLoop
 
 			# FIND NUMBER OF NEIGHBOR BOMBS
+			add $20, $0, $0
 			beq $11, $0, evaluate	# If the square's value is 0, skip to evaluate function
 			add $6, $0, $0
 nearBombs:	beq $20, $8 nearClose	# Loop until the index value is 7
@@ -132,7 +129,11 @@ nearClosed: beq $20, $8 check	# Loop until the index value is 7
 			addi $22, $22, 1	    # Add 1 to count of nearby closed squares
 			bne $20, $8 nearClosed	# Loop until the index value is 7
 
-check:		add $18, $6, $22	    # Finds total size
+check:		bne $22, $0, check2
+			sb $15, mainArr($12)	# Insert the value of $15 into the position to denote a square with fully known neighbors
+			j endif
+
+check2:		add $18, $6, $22	    # Finds total size
 			bne $18, $11, endif	    # If Totalsize is not equal, loop back
 			beq $0, $11, endif	    # If the current index equals zero, loop back
 			
@@ -141,9 +142,9 @@ evaluate:	add $14, $0, $0			# (Re-)Initialize index-er
 			sb $15, mainArr($12)	# Insert the value of $15 into the position to denote a square with fully known neighbors
 evald:		beq $14, $8, endif      # Loop until Run through all indexes
 			lbu $11 indexArr($14)   # Load the index of the neighbor square into $11
-			lbu $16, nearArr($14)   # Index at first neighbor value
+			lbu $20, nearArr($14)   # Index at first neighbor value
 			addi $14, $14, 1	    # Add 1 to the nearArr index-er
-			bne $16, $27 evald      # Check if the neighbor is closed, if not skip to next neighbor
+			bne $20, $27 evald      # Check if the neighbor is closed, if not skip to next neighbor
 			add $25, $11, $0
 			jal subtract
 
@@ -162,8 +163,12 @@ evalEnd:    swi   568               # returns result in $4 (9)
 
 endif:		beq $1, $5, end			# If all flags are found, end game
 			addi $10, $10, 1		# Add 1 to the counter 
-			addi $12, $12, 1		# Add 1 to the counter 
-			bne $10, $13, FlagOrOpen # Loop Flag until run through all squares availables
+			addi $12, $12, 1		# Add 1 to the index 
+			div $12, $15			# See if The new index is at the end of a line
+			mflo $23
+			bne $23, $9, lineSkip	# If it is, add another to get to the next index
+			addi $12, $12, 2		# Add 2 to the index because we jump to next line
+lineSkip:	bne $10, $13, FlagOrOpen # Loop Flag until run through all squares availables
 			bne $21, $0 nowCheck	# If this was the Open loop, go to nowCheck
 			addi $21, $0, 1
 			j resetTen
@@ -177,33 +182,16 @@ neighIndex:	lbu $18, mainArr($28)
 endNeigh:	jr $31
 
 			#Figure out how much to subtract
-subtract:	slti $6, $11, 0x4F
-			beq $6, $0, sub1
-			slti $6, $11, 0x45
-			beq $6, $0, sub7
-			slti $6, $11, 0x3B
-			beq $6, $0, sub6
-			slti $6, $11, 0x31
-			beq $6, $0, sub5
-			slti $6, $11, 0x24
-			beq $6, $0, sub4
-			slti $6, $11, 0x1D
-			beq $6, $0, sub3
-			slti $6, $11, 0x13
-			beq $6, $0, sub2
-			addiu $11, $11, -11
-			jr $31			
-sub7:		addiu $11, $11, -23
+subtract:	add $20, $0, $0
+			add $6, $0, $0
+
+sbubby:		lb $28, subArr($20)
+			slt $28, $11, $28
+			bne $28, $0, noSub
+			lb $28, subvalsArr($20)
+			add $11, $11, $28
+noSub:		addi $20, $20, 1
+			slti $6, $20, 8
+			bne $6, $0, sbubby
 			jr $31
-sub6:		addiu $11, $11, -21
-			jr $31
-sub5:		addiu $11, $11, -19
-			jr $31
-sub4:		addiu $11, $11, -17
-			jr $31
-sub3:		addiu $11, $11, -15
-			jr $31
-sub2:		addiu $11, $11, -13
-			jr $31
-sub1:		addiu $11, $11, -25
-			jr $31
+
